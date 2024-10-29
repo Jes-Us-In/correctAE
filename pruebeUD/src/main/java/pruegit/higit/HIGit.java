ca28ffa0ea41bbe5c92895cc3f4cc401cad3c13f
JESUS DEL BUEY JIMENEZ <Jesus.delBuey@uclm.es>
2024-10-29 14:33:02 +0100

    HiGit Pruebas

---
+++ pruebeUD/src/main/java/pruegit/higit/HIGit.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pruegit.higit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jesus.delBuey
+ */
+public class HIGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JOptionPane.showMessageDialog(null, "Saludos desde Git", "Pruebas en Git",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
